93c3dfd163088bb17172e3b35ca75dbd1fcbe1d2
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-25 15:37:12 +0000

    seedデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,10 @@
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
   password = "password"
+  photo = Faker::Avatar.image 
   User.create!(name: name,
               email: email,
               password: password,
+              photo: photo
               )
 end
